d1e5996c3706d507c394e31cd2eb6cf6448ecd51
prathamesh magar <prathameshma1@gmail.com>
2024-06-30 23:33:47 +0530

    OK

--- Evalution/PaymentManagement/Users/forms.py
+++ Evalution/PaymentManagement/Users/forms.py
@@ -5,6 +5,11 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         fields = ['service_user', 'service', 'amount']
+        widegets = {
+            'service_user': forms.Select(),
+            'service': forms.Select(),
+            'amount': forms.NumberInput()
+        }
 
 
 
